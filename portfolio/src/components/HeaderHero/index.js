1306f37b5c44cb7606389d0e23df42c6ad5c6e0f
Tomomi Inoue <73736986+Chib1co@users.noreply.github.com>
2021-04-28 19:52:43 +1000

    final change

--- portfolio/src/components/HeaderHero/index.js
+++ portfolio/src/components/HeaderHero/index.js
@@ -5,7 +5,7 @@ function HeaderHero(props) {
     return(
         <div className="header-img" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
             <div className="offset-1 col-9 " id="home"></div>
-            <h3 className="fade title">I am Web development student at BCS</h3>
+            <h3 className="fade title">Hello, I am Full Stack Web Developer</h3>
        {/* {props.children} */}
         {/* <div className="offset-1 col-9 " id="home"></div> */}
         </div>
